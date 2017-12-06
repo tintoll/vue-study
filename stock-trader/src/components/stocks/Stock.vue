59c19de8038dbf01cd466a7ca808a9e17d72f8f3
tintoll <blue@gmail.com>
2017-12-06 10:39:22 +0900

    stock 디자인 추가

--- stock-trader/src/components/stocks/Stock.vue
+++ stock-trader/src/components/stocks/Stock.vue
@@ -1,8 +1,64 @@
 <template>
-  <div>Stocks-Stock</div>
+  <div class="col-md-4 col-sm-6">
+    <div class="panel panel-success">
+      <div class="panel-heading">
+        <h3 class="panel-title">
+            {{ stock.name }}
+            <small>(Price: {{ stock.price }} )</small>
+        </h3>
+      </div>
+      <div class="panel-body">
+        <div class="pull-left">
+          <input type="number"
+                 class="form-control"
+                 placeholder="Quantity"
+                 v-model.number="quantity"
+          />
+          <!--
+            v-model 데이터를 연결해주는 역할을 한다.
+            .number는 무슨역할이지? 문자열을 숫자로 변경해준다.
+            -->
+        </div>
+        <div class="pull-right">
+          <!--
+            @click는 클릭이벤트를 나타냄. v-on:click의 단축형태이다.
+            :disabled 는 표현식값에 따라 true이면 사용못하게하고 false 사용되게해준다.
+           -->
+          <button
+                  class="btn btn-success"
+                  @click="buyStock"
+                  :disabled="quantity <= 0 || !Number.isInteger(quantity)"
+                  > Buy </button>
+        </div>
+      </div>
+
+    </div>
+  </div>
 </template>
 
 <script>
+  export default {
+    props : ['stock'], // 상위 컴포넌트에서 내려주는 Data
+
+    // 이 컴포넌트에서 사용할 Data
+    data() {
+        return {
+          quantity : 0
+        }
+    },
+    // 이 컴포넌트에서 사용할 methods
+    methods : {
+      buyStock() {
+        const order = {
+          stockId : this.stock.id,
+          stockPrice : this.stock.price,
+          quantity : this.quantity,
+        }
+        console.log(order);
+        this.quantity = 0;
+      }
+    }
+  }
 </script>
 
 <style>
--- stock-trader/src/components/stocks/Stocks.vue
+++ stock-trader/src/components/stocks/Stocks.vue
@@ -1,8 +1,37 @@
 <template>
-  <div>Stocks</div>
+  <div>
+    <!--
+      v-for는 반복을 시켜주는 역할을 함.
+      :key는 엘레멘트 순서를 바꿔주는 특수속성인데 커스텀El일때 v-for만사용하면 에러가 나서 추가해주었음
+      :stock는 하위 컴포넌트에 데이터를 내려줄때 사용함.
+      (v-bind:stock을 줄임 표현임.)
+    -->
+    <app-stock v-for="stock in stocks"
+               :key="stock.id"
+               v-bind:stock="stock"></app-stock>
+  </div>
 </template>
 
 <script>
+  import Stock from './Stock.vue';
+  export default {
+    data() {
+      return {
+        stocks : [
+          {id:1, name: 'BENZ', price : 200},
+          {id:2, name: 'AUDI', price : 150},
+          {id:3, name: 'HONDA', price : 180},
+          {id:4, name: 'LEXUS', price : 195},
+        ]
+      }
+    },
+
+    // 사용할 컴포넌트를 등록해준다.
+    // camelCase로 등록해주면 template에서는 kebab-case로 사용하면된다.
+    components : {
+      appStock : Stock
+    }
+  }
 </script>
 
 <style>
